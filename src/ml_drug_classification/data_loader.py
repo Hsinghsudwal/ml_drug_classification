2cc05fbadd9964a25ed99b8f770be139432e7a8d
Harinder Singh Sudwal <sudwalh@gmail.com>
2025-02-05 16:08:17 -0800

    loader data file

--- src/ml_drug_classification/data_loader.py
+++ src/ml_drug_classification/data_loader.py
@@ -5,24 +5,31 @@ from src.utility import *
 from src.constants import *
 import os
 
+
 class DataLoader:
     def __init__(self):
         pass
 
     def dataLoader(self, path):
         try:
-            
-            data=Utility.read_data(path)
-            
+
+            data = Utility.read_data(path)
+
             # split into train test data
-            train_data, test_data = train_test_split(data, test_size= TEST_SIZE, random_state=42)
+            train_data, test_data = train_test_split(
+                data, test_size=TEST_SIZE, random_state=42
+            )
             # create folder to store data
-            raw_data_path = os.path.join(OUTPUT,RAW_FOLDER)
-            os.makedirs(raw_data_path,exist_ok=True)
+            raw_data_path = os.path.join(OUTPUT, RAW_FOLDER)
+            os.makedirs(raw_data_path, exist_ok=True)
             # save train test to csv
-            train_data.to_csv(os.path.join(raw_data_path,str(TRAIN_DATA_FILE)),index=False)
-            test_data.to_csv(os.path.join(raw_data_path,str(TEST_DATA_FILE)),index=False)
-            print('Data Ingestion Completed')
+            train_data.to_csv(
+                os.path.join(raw_data_path, str(TRAIN_DATA_FILE)), index=False
+            )
+            test_data.to_csv(
+                os.path.join(raw_data_path, str(TEST_DATA_FILE)), index=False
+            )
+            print("Data Ingestion Completed")
             return train_data, test_data
 
         except Exception as e:
