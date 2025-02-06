d393b44fafc25c0a0b552ebdf364e06830540c2a
Harinder Singh Sudwal <sudwalh@gmail.com>
2025-02-05 16:08:54 -0800

    data validate

--- src/ml_drug_classification/data_validation.py
+++ src/ml_drug_classification/data_validation.py
@@ -11,28 +11,33 @@ class DataValidation:
     def __init__(self) -> None:
         pass
 
-    def data_validation(
-        self, train_data, test_data):
+    def data_validation(self, train_data, test_data):
         try:
 
             data_results = {}
             for feature in train_data.columns:
                 ks_stat, p_value = ks_2samp(train_data[feature], test_data[feature])
                 data_results[feature] = p_value
-                
+
                 if p_value < THRESHOLD:
                     return "Error drift data"
-                
-                else:
-                    validate_data_path = os.path.join(OUTPUT,VALIDATE_FOLDER)
-                    os.makedirs(validate_data_path,exist_ok=True)
-                    # save train test to csv
-                    train_data.to_csv(os.path.join(validate_data_path,str(TRAIN_DATA_FILE)),index=False)
-                    test_data.to_csv(os.path.join(validate_data_path,str(TEST_DATA_FILE)),index=False)
-                    report_file=os.path.join(validate_data_path,REPORT_JSON)
 
-                    with open(report_file,'w') as f_in:
-                        json.dump(data_results,f_in,indent=4)
+                else:
+                    validate_data_path = os.path.join(OUTPUT, VALIDATE_FOLDER)
+                    os.makedirs(validate_data_path, exist_ok=True)
+                    # save train test to csv
+                    train_data.to_csv(
+                        os.path.join(validate_data_path, str(TRAIN_DATA_FILE)),
+                        index=False,
+                    )
+                    test_data.to_csv(
+                        os.path.join(validate_data_path, str(TEST_DATA_FILE)),
+                        index=False,
+                    )
+                    report_file = os.path.join(validate_data_path, REPORT_JSON)
+
+                    with open(report_file, "w") as f_in:
+                        json.dump(data_results, f_in, indent=4)
 
                     print("Data validation completed")
                     return train_data, test_data
